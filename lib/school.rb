ba159c6eeb5b420c9584cc18b6ff6d6f0fdce6c8
Jillian Weimer <jilliannweimer@gmail.com>
2018-04-17 00:01:08 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,8 +7,24 @@ def initialize(name)
   @roster = {}
 end
 
+def add_student(student_name, grade)
+    roster[grade] ||= []
+    roster[grade] << student_name
+  end
 
-school.roster
+  def grade(student_grade)
+    roster[student_grade]
+  end
+
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
+  end
+end
 
 
 
